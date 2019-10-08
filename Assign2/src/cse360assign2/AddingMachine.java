17fe6d69b3184bac56439cbb1084101f2d93f641
Dustin Allen <dwallen3@asu.edu>
2019-10-08 16:20:25 -0700

    initial version

---
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,39 @@
+/*
+ * Dustin Allen, Side R Row E seat 10, Assignment 2
+ * this file contains the class and methods that we will be using for this
+ * assignment in which we are using github to make a repository using this file
+ */
+package cse360assign2;
+/*
+ * The purpose of this class is to do all of the operations of the methods in 
+ * this class
+ */
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () { // the object to add all the operations
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () { // returns the number stored in the total
+		return 0;
+	}
+	
+	public void add (int value) { // adds to total
+		
+	}
+	
+	public void subtract (int value) { // subtracts from total
+		
+	}
+	
+	public String toString () { // returns string of all the math operations
+		return "";
+	}
+	
+	public void clear() { // clears string
+		
+	}
+
+}
